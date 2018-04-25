158b1b171de613beea2456bab14e5b829db59b8b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-04-25 18:15:18 +0000

    Deploy vladosiv/shri-homework-task1 to github.com/vladosiv/shri-homework-task1.git:gh-pages

--- src/reducers/mainReducer.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import {feedReducer} from './feedReducer';
-import {modalReducer} from './modalReducer';
-
-export const mainReducer = combineReducers({
-	feed: feedReducer,
-	modal: modalReducer,
-});
