3b81b29e6cd259b40ea0bb75d4844ce4a89c12c4
Vladislav Sivitsky <vladosiv@gmail.com>
2018-04-26 17:06:01 +0300

    test environment

---
+++ src/tests/test.js
@@ -0,0 +1,13 @@
+import chai from 'chai'
+import React from 'react'
+import Header from '../components/Header/Header.jsx'
+import {shallow} from 'enzyme'
+
+let expect = chai.expect
+describe("<Header/>", ()=>{
+    "use strict";
+    it('renders one <header> tag', ()=>{
+        const wrapper = shallow(<Header />);
+        expect(wrapper.find('header')).to.have.length(1)
+    });
+})
---
+++ src/tests/testSetup.js
@@ -0,0 +1,3 @@
+import {shallow, configure} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
